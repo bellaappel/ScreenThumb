6d13da3cf0841c3140b1a528d3bb1ac52d54094e
bellaappel <bellaappel0@gmail.com>
2020-12-16 13:07:56 -0500

    form for creating user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :plants
-  resources :sessions, only: :[create]
+  resources :sessions, only: [:create]
   get '/login', to: 'sessions#login', as: 'login'
   get '/logout', to: 'sessions#destroy', as: 'logout'
 end
