6ee8dc4b99ed8ca3fd3f5a495fdfb531cd43ac18
bellaappel <bellaappel0@gmail.com>
2020-12-16 12:22:51 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :plants
+  resources :sessions, only: :[create]
+  get '/login', to: 'sessions#login', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 end
