bc5f6dd6e57060f48569609f5cddc32882d79a1f
bellaappel <bellaappel0@gmail.com>
2020-12-17 14:31:54 -0500

    can login & create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,41 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
-
+    
     def show
     end
 
-    def new
-        @user = User.new
+   def signup
+    @user = User.new
+   end
+
+   def login
+    @user = User.new
+   end
+
+
+  def create_session
+    user = User.find_by(username: params[:user][:username])
+
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path
+    else
+      redirect_to '/signup', notice: "Oops something isn't right"
     end
+  end
+
 
     def edit
     end
 
     def create
-        @user = User.new(user_params)
-        if user.password == user.password_confirmation
-            @user.save
-            session[:user_id] = @user.id
-            redirect_to @user
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            redirect_to user_path(user.id)
         else
-            render :new, notice: "You're passwords don't match!"
+            redirect_to '/signup'
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, except: [:index]
   resources :plants
   resources :sessions, only: [:create]
-  get '/login', to: 'sessions#login', as: 'login'
+  get '/login', to: 'users#login', as: 'login'
+  post '/login', to: 'users#create_session'
   get '/logout', to: 'sessions#destroy', as: 'logout'
+  get '/signup', to: 'users#signup', as: 'new'
+ 
 end
