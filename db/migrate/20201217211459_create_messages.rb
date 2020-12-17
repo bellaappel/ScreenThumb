5143a805b2df9a99ddab4a3b56232da30fe8e133
bellaappel <bellaappel0@gmail.com>
2020-12-17 16:16:13 -0500

    messages

---
+++ db/migrate/20201217211459_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :messages do |t|
+      t.integer :sender_id
+      t.integer :reciepient_id
+      t.timestamp :created_at
+      t.text :content
+      t.boolean :read
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_182718) do
+ActiveRecord::Schema.define(version: 2020_12_17_211459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2020_12_16_182718) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "reciepient_id"
+    t.datetime "created_at"
+    t.text "content"
+    t.boolean "read"
   end
 
   create_table "plant_types", force: :cascade do |t|
