af0bc5e159185660f93fec04dcb476e2a41c008b
bellaappel <bellaappel0@gmail.com>
2020-12-17 13:02:55 -0500

    render partial in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if user.password == user.password_confirmation
-        if @user.save
+            @user.save
             session[:user_id] = @user.id
             redirect_to @user
         else
@@ -46,5 +46,5 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password, :password_confirmation)
-    end
+    end   
 end
