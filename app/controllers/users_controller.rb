4c476bdb094340373c19d6b972c353eb0be6ce58
bellaappel <bellaappel0@gmail.com>
2020-12-17 15:52:07 -0500

    fixed login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
+   
     def index
         @users = User.all
     end
     
     def show
+        @user = User.find_by(id: params[:id])
     end
 
    def signup
@@ -55,11 +56,6 @@ class UsersController < ApplicationController
     end
 
     private
-
-    def set_user
-        @user = User.find(params[:id])
-    end
-
     def user_params
         params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end   
