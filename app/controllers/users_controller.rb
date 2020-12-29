99b2b6579ceec56f38905031caa062299ed7e5fb
bellaappel <bellaappel0@gmail.com>
2020-12-29 14:26:02 -0500

    edit working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,11 +48,8 @@ class UsersController < ApplicationController
 
     def update
         @user = User.find_by(id: params[:id])
-        if @user.update(user_params)
-            redirect to @user
-        else 
-            render :edit
-        end
+        @user.update(user_params)
+        redirect_to user_path(@user.id)
     end
 
     def destroy
