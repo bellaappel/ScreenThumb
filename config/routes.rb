99001385f821fb0df0d18ea62fd0f4d4729e6644
bellaappel <bellaappel0@gmail.com>
2021-01-01 17:26:08 -0500

    maybe?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'application#home'
+  get '/', to: "application#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/plant-with-most-comments', to: 'plants#most_comments', as: 'most_commented_plant'
   resources :users, except: [:index]
